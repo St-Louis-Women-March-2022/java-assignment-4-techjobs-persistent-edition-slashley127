e519e7d610cfb711de3c866dab35fe2b320a4b3b
unknown <slashley127@gmail.com>
2022-11-22 10:31:06 -0600

    working through task 3

--- src/main/java/org/launchcode/techjobs/persistent/models/Job.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Job.java
@@ -3,41 +3,24 @@ package org.launchcode.techjobs.persistent.models;
 import javax.persistence.*;
 
 @Entity
-public class Job{
+public class Job extends AbstractEntity{
 
-    @Id
-    @GeneratedValue
-    private int id;
-
-    private String name;
-
-    private String employer;
+    private Employer employer;
     private String skills;
 
-    public Job() {
+    public Job() {}
+
+    public Job(Employer employer, String skills) {
+        this.employer = employer;
+        this.skills = skills;
     }
 
-    public Job(String anEmployer, String someSkills) {
-        super();
-        this.employer = anEmployer;
-        this.skills = someSkills;
-    }
 
-    // Getters and setters.
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmployer() {
+    public Employer getEmployer() {
         return employer;
     }
 
-    public void setEmployer(String employer) {
+    public void setEmployer(Employer employer) {
         this.employer = employer;
     }
 
